5c9d458c0b85365bb502403f4465b19c9225d294
Sidharth <sidharthsuresh07@gmail.com>
2026-01-25 23:54:21 -0500

    worked on two scripts and updated the scene

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -1,9 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Inventory
 {
     public class Inventory : MonoBehaviour
     {
+        [SerializeField] private ItemizedObject defaultItem; // the arm
         
+        private int maxCapacity = 3;
+
+        private int currentlyHoldingIndex = 0;
+        
+        // item id : itemized object class
+        private Dictionary<string, ItemizedObject> items = new Dictionary<string, ItemizedObject>();
+        
+        public void AddItem(ItemizedObject item)
+        {
+            items.Add(item.GetItemName(), item);
+        }
+
+        public void RemoveItem(ItemizedObject item)
+        {
+            items.Remove(item.GetItemName());
+        }
     }
 }
--- Assets/Scripts/Inventory/ItemizedObject.cs
+++ Assets/Scripts/Inventory/ItemizedObject.cs
@@ -4,6 +4,33 @@ namespace Inventory
 {
     public class ItemizedObject : MonoBehaviour
     {
+        private string itemName;
+        private GameObject prefab;
+
+        public GameObject GetPrefab()
+        {
+            return prefab;
+        }
+
+        private void SetPrefab(GameObject prefab)
+        {
+            this.prefab = prefab;
+        }
+
+        public string GetItemName()
+        {
+            return itemName;
+        }
+
+        public void SetItemName(string itemName)
+        {
+            this.itemName = itemName;
+        }
+
+        private void OnUse()
+        {
+            // do something (must be overwritten by the child class)
+        }
         
     }
 }
