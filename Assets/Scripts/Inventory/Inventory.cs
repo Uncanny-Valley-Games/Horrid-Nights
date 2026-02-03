09a762d97272a8b3a1dcf60b46e04639438fda29
Sidharth <sidharthsuresh07@gmail.com>
2026-02-03 04:11:24 -0500

    added some code to inventory class

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] private GameObject dropBoxPrefab;
+    
+    private const int INVENTORY_SIZE = 3;
+    
+    private InventoryItem[] items = new InventoryItem[INVENTORY_SIZE]; // can hold a maximum of 3 items
+    
     void Start()
     {
         
