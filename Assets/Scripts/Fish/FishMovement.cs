113c89537f7af4fd41fe58ae2e721de4d44b8e84
ChrisTheCoder27 <christopher.dupas@ontariotechu.net>
2026-02-11 11:26:37 -0500

    Added basic movement for fish

--- Assets/Scripts/Fish/FishMovement.cs
+++ Assets/Scripts/Fish/FishMovement.cs
@@ -3,14 +3,40 @@ using UnityEngine;
 public class FishMovement : MonoBehaviour
 {
     [SerializeField] Transform fishPoint;
+    [SerializeField] Transform fishPoint2;
+
+    Transform currentWaypoint;
+    Vector3 direction;
 
     void Start()
     {
-        
+        currentWaypoint = fishPoint;
+        direction = Vector3.Normalize(currentWaypoint.position - transform.position);
+    }
+
+    void Update()
+    {
+        // Makes the fish spin, leaving it commented for now
+        //transform.Rotate(direction);
     }
 
     void FixedUpdate()
     {
-        
+        transform.Translate(direction * 1.5f * Time.deltaTime);
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (currentWaypoint == fishPoint)
+        {
+            currentWaypoint = fishPoint2;
+            direction = Vector3.Normalize(currentWaypoint.position - transform.position);
+        }
+        else
+        {
+            currentWaypoint = fishPoint;
+            direction = Vector3.Normalize(currentWaypoint.position - transform.position);
+        }
+    }
+
 }
