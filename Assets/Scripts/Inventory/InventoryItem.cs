28d40135dff7da46197574b6411888897641b2a7
Sidharth <sidharthsuresh07@gmail.com>
2026-02-22 14:04:24 -0500

    added test textures and inventory stuff with a smidge of the fishing mini game prep work

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class InventoryItem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public enum ItemName
+    {
+        Nothing,
+        Axe,
+        FishingRod,
+        Hammer
+    }
+    
+    public ItemName itemName;
+    public Collider _collider;
+
+    private Rigidbody _rb;
+    private bool isEquipped;
+
+    public bool IsEquipped()
+    {
+        return isEquipped;
+    }
+    
     void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true; // starts as a static object, then switches to ridgid once dropped
+    }
+
+    
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Drop()
     {
-        
+        isEquipped = false;
+        _rb.isKinematic = false;
     }
 }
